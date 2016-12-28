d3f6357b57beda1478487dda828d50c0bcd0515c
jasonb22 <jason.buchanan@us.usana.com>
2016-12-28 14:48:52 -0700

    Refactoring Text101 state code to be a little more readable and learning about C# Dictionaries, KeyValuePair and var.

--- Text101/Assets/TextController.cs
+++ Text101/Assets/TextController.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
+using System.Collections.Generic;
 
 public class TextController : MonoBehaviour {
 
@@ -28,67 +28,111 @@ public class TextController : MonoBehaviour {
 		else if (myState == States.freedom) {displayFreedom();}
 	}
 	
-	void displayCell() {
+	private void displayCell() {
 		text.text = "You are in a prison cell, and you want to escape. There are " +
 					"some dirty sheets on the bed, a mirror on the wall, and the door " +
 					"is locked from the outside.\n\n" +
 					"Press S to view Sheets, M to view Mirror and L to view Lock" ;
-		if (Input.GetKeyDown(KeyCode.S)) {myState = States.sheets;}
-		else if (Input.GetKeyDown(KeyCode.M)) {myState = States.mirror;}
-		else if (Input.GetKeyDown(KeyCode.L)) {myState = States.small_lock;}
+		
+		handleChoice(new Dictionary<KeyCode, States>(){
+			{KeyCode.S, States.sheets},
+			{KeyCode.M, States.mirror},
+			{KeyCode.L, States.small_lock}
+		});
 	}
 	
-	void displayCellWhileHoldingMirror() {
+	private void displayCellWhileHoldingMirror() {
 		text.text = "You are still in your cell, and you STILL want to escape! There are " +
 					"some dirty sheets on the bed, a mark where the mirror was, " +
 					"and that pesky door is still there, and firmly locked!\n\n" +
-					"Press S to view Sheets, or L to view Lock" ;
-		if (Input.GetKeyDown(KeyCode.S)) {myState = States.sheets_1;}
-		else if (Input.GetKeyDown(KeyCode.L)) {myState = States.lock_1;}
+					"Press S to view Sheets, or L to view Lock";
+					
+		handleChoice(new Dictionary<KeyCode, States>(){
+			{KeyCode.S, States.sheets},
+			{KeyCode.L, States.small_lock_to_mirror}
+		});					
 	}
 	
-	void displayFreedom() {
+	private void displayFreedom() {
 		text.text = "You are FREE!\n\n" +
 					"Press P to Play again";
-		if (Input.GetKeyDown(KeyCode.P)) {myState = States.cell;}
+
+		handleReturnToCell(KeyCode.P);
 	}
 	
-	void viewSheets() { 
+	private void viewSheets() { 
 		text.text = "You can't believe you sleep in these things. Surely it's " +
 					"time somebody changed them. The pleasures of prison life " +
 					"I guess!\n\n" +
 					"Press R to Return to roaming your cell" ;
-		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
+		
+		handleReturnToCell();
 	}
 	
-	void viewLock() {
+	private void viewLock() {
 		text.text = "This is one of those button locks. You have no idea what the " +
 					"combination is. You wish you could somehow see where the dirty " +
 					"fingerprints were, maybe that would help.\n\n" +
 					"Press R to Return to roaming your cell" ;
-		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
+		
+		handleReturnToCell();					
 	}
 	
-	void viewMirror() {
+	private void viewMirror() {
 		text.text = "The dirty old mirror on the wall seems loose.\n\n" +
 					"Press T to Take the mirror, or R to Return to cell" ;
-		if (Input.GetKeyDown(KeyCode.T)) {myState = States.cell_holding_mirror;}
-		else if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell;}
+		handleChoice(new Dictionary<KeyCode, States>(){
+			{KeyCode.T, States.cell_holding_mirror},
+			{KeyCode.R, States.cell}
+		});
 	}
 	
-	void holdMirrorToSheets() {
+	//todo: bug we never get to here
+	private void holdMirrorToSheets() {
 		text.text = "Holding a mirror in your hand doesn't make the sheets look " +
 					"any better.\n\n" +
 					"Press R to Return to roaming your cell" ;
-		if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_holding_mirror;}
+		
+		handleChoice(getCommand(KeyCode.R, States.cell_holding_mirror));
 	}
 	
-	void holdMirrorToLock() {
+	private KeyValuePair<KeyCode, States> getCommand(KeyCode key, States state) { 
+		return new KeyValuePair<KeyCode, States>(key, state);
+	}
+	
+	private void holdMirrorToLock() {
 		text.text = "You carefully put the mirror through the bars, and turn it round " +
 					"so you can see the lock. You can just make out fingerprints around " +
 					"the buttons. You press the dirty buttons, and hear a click.\n\n" +
 					"Press O to Open, or R to Return to your cell" ;
-		if (Input.GetKeyDown(KeyCode.O)) {myState = States.freedom;}
-		else if (Input.GetKeyDown(KeyCode.R)) {myState = States.cell_mirror;}
+		
+		handleChoice(new Dictionary<KeyCode, States>(){
+			{KeyCode.O, States.freedom}, 
+			{KeyCode.R, States.cell_holding_mirror}
+		});
+	}
+	
+	private void handleReturnToCell() {
+		handleReturnToCell(KeyCode.R);
+	}
+	
+	private void handleReturnToCell(KeyCode key) {
+		handleChoice(getCommand(key, States.cell));
+	}
+	
+	private void handleChoice(IDictionary<KeyCode, States> commands) {
+		foreach (var command in commands) {
+			if (handleChoice(command)) {
+				break;
+			}
+		}
+	}	
+	
+	private bool handleChoice(KeyValuePair<KeyCode, States> command) {
+		bool commandExecuted = Input.GetKeyDown(command.Key);
+		if (commandExecuted) {
+			myState = command.Value;
+		}
+		return commandExecuted;
 	}
 }
